0d4bf6d5182f883bb6ab887110cff5401fd174e6
Josiah Hollibaugh <jo.hollibaugh@gmail.com>
2025-03-09 14:02:14 -0500

    Init Setup = getting response from KTS

---
+++ ToneStudioAutomation/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+Console.WriteLine("Hello, World!");
+
+var options = new ChromeOptions();
+options.DebuggerAddress = "localhost:9222"; // Attach to running Tone Studio
+var driver = new ChromeDriver(, options);
+using (var driver = new ChromeDriver(@"C:\Path\To\OldChromeDriver", options))
+{
+    try
+    {
+        var gainInput = driver.FindElement(By.CssSelector("#panel-amp-gain-spinner input"));
+        string gainValue = gainInput.GetAttribute("value");
+
+        Console.WriteLine($"Gain: {gainValue}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
