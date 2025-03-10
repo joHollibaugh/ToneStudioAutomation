63d317a785c0ee2a68b5b5dc7efc6d7821ec78df
Josiah Hollibaugh <jo.hollibaugh@gmail.com>
2025-03-09 20:47:03 -0500

    Replace Selenium / MSAutoUI to pupeteer - working session / element scraping

--- ToneStudioAutomation/Program.cs
+++ ToneStudioAutomation/Program.cs
@@ -2,85 +2,10 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.DevTools;
-
-// 1. Powershell : cd "C:\Program Files (x86)\BOSS\BOSS TONE STUDIO for KATANA MkII"
-//      |_ .\"BOSS TONE STUDIO for KATANA MkII.exe" --remote-debugging-port=9223
-// 2.    |_cd "C:\Program Files\Google\Chrome\Application"
-//          |_ .\chrome.exe --remote-debugging-port=9223 --user-data-dir="C:\ChromeDebug"
-string devToolsUrl = "http://localhost:9223/json"; // DevTools API endpoint
-
-using (HttpClient client = new HttpClient())
-{
-    try
-    {
-        string response = await client.GetStringAsync(devToolsUrl);
-        var sessions = JsonSerializer.Deserialize<JsonElement>(response);
-
-        foreach (var session in sessions.EnumerateArray())
-        {
-            // Returns
-            // [ {
-            //     "description": "",
-            //     "devtoolsFrontendUrl": "/devtools/inspector.html?ws=localhost:9223/devtools/page/E418E65EA0C405E7505357A3E9B5D5FB",
-            //     "id": "E418E65EA0C405E7505357A3E9B5D5FB",
-            //     "title": "BOSS TONE STUDIO for KATANA Mk II",
-            //     "type": "page",
-            //     "url": "file:///C:/Users/Josiah Hollibaugh/AppData/Local/Roland/BOSS TONE STUDIO for KATANA MkII/html/index.html",
-            //     "webSocketDebuggerUrl": "ws://localhost:9223/devtools/page/E418E65EA0C405E7505357A3E9B5D5FB"
-            // } ]
-            
-                string debuggerUrl = session.GetProperty("webSocketDebuggerUrl").GetString();
-                
-                
-                var options = new ChromeOptions();
-                options.DebuggerAddress = debuggerUrl;
-                using (ChromeDriver driver = new ChromeDriver(options))
-                {
-                    // Execute Chrome DevTools Protocol Command to get the DOM tree
-                    var toneStudioTree = driver.ExecuteCdpCommand("DOM.getDocument", new Dictionary<string, object>
-                    {
-                        { "depth", -1 }  // -1 means full depth
-                    });
-
-                    File.WriteAllText("C:\\Users\\Josiah Hollibaugh\\Documents\\KTNAOUT\rslt.txt", toneStudioTree.ToString());
-                }
-            
-            
-        }
-
-        Console.WriteLine("No active session found for Katana Tone Studio.");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine("Error fetching DevTools sessions: " + ex.Message);
-    }
-}
+using ToneStudioAutomation;
 
 
-// ChromeOptions options = new ChromeOptions();
-// options.UseWebSocketUrl = true;
+//await SeleniumScraper.RunAsync();
 
+await PuppeteerAutomation.RunAsync();
 
-//using (ChromeDriver driver = new ChromeDriver(options))
-//{
-// driver.Navigate().GoToUrl("devtools://devtools/bundled/inspector.html?remoteFrontend=true");
-// Get the DevTools session
-//var devTools = (driver as IDevTools).GetDevToolsSession();
-// var ses = devTools.GetVersionSpecificDomains<DevToolsSessionDomains>(); 
-// Enable the console events
-// devTools.Runtime.ConsoleAPICalled += (sender, e) =>
-// {
-//     Console.WriteLine($"Console output: {e.Message.Text}");
-// };
-// Inject JavaScript into the DevTools console
-// string script = "console.log('JavaScript executed in DevTools Console');";
-// devTools.Runtime.Evaluate(new EvaluateCommand
-// {
-//     Expression = script
-// });
-// This will log "JavaScript executed in DevTools Console" in your output
-// Console.ReadLine();
-//}
-
-
-// Att 2 : chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\ChromeDebug"
---
+++ ToneStudioAutomation/Pupeteer/TonseStudioScraper.cs
@@ -0,0 +1,58 @@
+﻿using PuppeteerSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace ToneStudioAutomation;
+
+public static class PuppeteerAutomation
+{
+    public static async Task RunAsync()
+    {
+        // Here, we specify that we want Chrome and set a custom download folder.string chromedriverPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "chromedriver.exe");
+        string chromedriverPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "chromedriver.exe");
+
+        var fetcherOptions = new BrowserFetcherOptions
+        {
+            Browser = SupportedBrowser.Chrome,
+            // This path will be combined with your project base directory.
+            Path = chromedriverPath
+        };
+        var browserFetcher = Puppeteer.CreateBrowserFetcher(fetcherOptions);
+        //await browserFetcher.DownloadAsync(); // This will ensure Chrome is downloaded        var browserFetcher = Puppeteer.CreateBrowserFetcher(fetcherOptions);
+
+        var launchOptions = new LaunchOptions
+        {
+            ExecutablePath = @"C:\Program Files\Google\Chrome\Application\chrome.exe", // Adjust if needed
+            Headless = false, // Set to false to see the UI
+            Args = new[]
+            {
+                "--allow-file-access-from-files",
+                "--disable-web-security",
+                "--disable-site-isolation-trials"
+            }
+        };
+
+        var browser = await Puppeteer.LaunchAsync(launchOptions);
+
+
+        var page = await browser.NewPageAsync();
+        string fileUrl =
+            "file:///C:/Users/Josiah%20Hollibaugh/AppData/Local/Roland/BOSS%20TONE%20STUDIO%20for%20KATANA%20MkII/html/index.html";
+        await page.GoToAsync(fileUrl);
+        Console.WriteLine("Navigated to Tone Studio page (Puppeteer).");
+
+        // Inject jQuery if it's not already present
+        bool hasJQuery = await page.EvaluateExpressionAsync<bool>("!!window.jQuery");
+        if (!hasJQuery)
+        {
+            Console.WriteLine("Injecting jQuery...");
+            await page.AddScriptTagAsync(new AddTagOptions { Url = "https://code.jquery.com/jquery-3.6.0.min.js" });
+        }
+
+        // Run a jQuery snippet to extract the GAIN value
+        string gainValue = await page.EvaluateExpressionAsync<string>(@"(function(){
+                    return $('#panel-amp-gain-spinner input').val();
+                })()");
+        Console.WriteLine($"Puppeteer: GAIN Value = {gainValue}");
+    }
+}
