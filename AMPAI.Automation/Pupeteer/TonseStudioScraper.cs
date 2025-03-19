8466430d2e92e800d7a348f04e26b56d26f382ea
Josiah Hollibaugh <jo.hollibaugh@gmail.com>
2025-03-19 03:00:54 -0500

    Max session timeout in PuppeteerAutomation - cleanup some comments and add TODOs

--- AMPAI.Automation/Pupeteer/TonseStudioScraper.cs
+++ AMPAI.Automation/Pupeteer/TonseStudioScraper.cs
@@ -7,18 +7,27 @@ namespace ToneStudioAutomation;
 
 public static class PuppeteerAutomation
 {
+    /*  TODO
+         - Automate startup for chrome / dev tools / tone studio (currently using powershell:
+                    1. .\"C:\Program Files (x86)\BOSS\BOSS TONE STUDIO for KATANA MkII\BOSS TONE STUDIO for KATANA MkII.exe" --remote-debugging-port={default is 9223}
+                    2. .\"C:\Program Files\Google\Chrome\Application\chrome.exe" --disable-web-security --allow-file-access-from-files --disable-site-isolation-trials --disable-cors
+                    3. Chrome needs port forwarding enabled (chrome://inspect/#devices should show "BOSS TONE STUDIO for KATANA Mk II" under "Remote Target")
+                    *********************************************    EXAMPLE   ***************************************************************************** 
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "powershell",
+                        Arguments = "-NoProfile -ExecutionPolicy Bypass -Command \"& './BOSS TONE STUDIO for KATANA MkII.exe' --remote-debugging-port=9222\"",
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                     });
+                     ****************************************************************************************************************************************
+         - Implement event consumer to start / stop the service, collect output
+         - Populate and output AMPAI.Core models (AmpSettingSnapshot); no file output here   
+    */ 
     public static async Task RunAsync()
     {
-        // Use PS to start tone studio - machine specific for path / version
-        // Process.Start(new ProcessStartInfo
-        // {
-        //     FileName = "powershell",
-        //     Arguments = "-NoProfile -ExecutionPolicy Bypass -Command \"& './BOSS TONE STUDIO for KATANA MkII.exe' --remote-debugging-port=9222\"",
-        //     UseShellExecute = false,
-        //     CreateNoWindow = true
-        // });
         string wsUrl = "";
-        string devToolsUrl = "http://localhost:9222/json"; // DevTools API endpoint
+        string devToolsUrl = "http://localhost:9223/json"; // DevTools API endpoint
         using (HttpClient client = new HttpClient())
         {
             string response = await client.GetStringAsync(devToolsUrl);
@@ -32,7 +41,7 @@ public static class PuppeteerAutomation
             //     "type": "page",
             //     "url": "file:///{$AppData}/Local/Roland/BOSS TONE STUDIO for KATANA MkII/html/index.html",
             //     "webSocketDebuggerUrl": "ws://localhost:9223/devtools/page/E418E65EA0C405E7505357A3E9B5D5FB"
-            // } ]
+            // }]
             foreach (var session in sessions.EnumerateArray())
             {
                 if (session.GetProperty("title").GetString() == "BOSS TONE STUDIO for KATANA Mk II")
@@ -42,27 +51,24 @@ public static class PuppeteerAutomation
                 }
             }
         }
-
         var browser = await Puppeteer.ConnectAsync(new ConnectOptions
         {
             IgnoreHTTPSErrors = true,
             BrowserWSEndpoint = wsUrl,
+            KeepAliveInterval =0000999999
         });
         var pages = await browser.PagesAsync();
         var page = pages[0]; // Select the correct tab
-
-        // Inject jQuery if it's not already present
+        
         bool hasJQuery = await page.EvaluateExpressionAsync<bool>("!!window.jQuery");
         if (!hasJQuery)
         {
             Console.WriteLine("Injecting jQuery...");
             await page.AddScriptTagAsync(new AddTagOptions { Url = "https://code.jquery.com/jquery-3.6.0.min.js" });
         }
-
-        // TODO - *replace with orchestrator context - publish event to cascade sync / stamp / write ops with recording 
-        //          ** Handle session closed exceptions
+        
         Console.WriteLine("Puppeteer connected to CEF devtools successfully - recording inputs every 5 seconds.");
-        string outputPath = @"C:\Users\Josiah Hollibaugh\Documents\KTNAOUT\ToneStudioData.txt";
+        string outputPath = @"%AppData%\AMPAI\TSSnapshot.txt";
         using (StreamWriter writer = new StreamWriter(outputPath, append: true))
         {
             while (true)
@@ -81,7 +87,6 @@ public static class PuppeteerAutomation
                 {
                     Console.WriteLine($"Puppeteer Exception occurred: {ex.Message}");
                 }
-
                 await Task.Delay(5000);
             }
         }
